4223d0866055da13a58cc76a1c7bcb781e5651e8
Jobbazs <grega.balazs@gmail.com>
2026-05-31 08:35:43 +0200

    url lecserélve

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => ['https://formulaonestatweb-k3aza3756-jobbazs-projects.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
